382ca2c7ff07341b4f0dd2da8c21c3c377b19899
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-22 00:59:58 +0100

    Printing the users names based on conditions

--- num_25.py
+++ num_25.py
@@ -1,6 +1,12 @@
 # To get the length of users name
-user_input = "enter your name: "
+user_input = input("enter your name: ")
 len_user_input = len(user_input)
 
-
-
+# if the length of the users name is less than 5
+# we would tell them to add their surname and print both of them together in uppercase
+if len_user_input < 5:
+    a = (input("Enter your Surname: "))
+    b = f"{user_input}{a}".upper()
+    print(b)
+else:  # else, if it is higher than 5, we would print it in lowercase
+    print(user_input.lower())
