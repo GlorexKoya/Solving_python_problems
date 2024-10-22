8b190033688c68673559aeddd2d91497832bf728
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-22 23:16:09 +0100

    "Testing"

--- num41.py
+++ num41.py
@@ -1,7 +1,7 @@
-# user_name_input = input("Enter your name: ")
-# user_num_input = int(input("Enter a number: "))
-#
-# if user_num_input < 10:
-#     for i in user_name_input:
-#         print(i)
+user_name_input = input("Enter your name: ")
+user_num_input = int(input("Enter a number: "))
+
+if user_num_input < 10:
+    for i in user_name_input:
+        print(i)
 
