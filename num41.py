903d842b3b32682d03fd6b0160e71b42f334e083
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-22 23:39:09 +0100

    "Code to ask for users name and a random number of their choice below 10."

--- num41.py
+++ num41.py
@@ -4,12 +4,12 @@ if they pick a number below 10, it would print their name that amount of times
 Else anything number higher than 10 it would print "Too high three times"
 """
 
-user_name_input = input("Enter your name: ")
-user_num_input = int(input("Enter a number: "))
+name_input = input("Enter your name: ")
+num_input = int(input("Enter a number: "))
 
-if user_num_input < 10:
-    for i in range(user_num_input):
-        print(user_name_input)
+if num_input < 10:
+    for i in range(num_input):
+        print(name_input)
 else:
     for x in range(3):
         print("Too high!")
