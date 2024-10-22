a22c4961136f7f15a261844779bc153e6eb28ee2
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-22 23:23:05 +0100

    "Code to convert a user input word to pig latin"

--- num26.py
+++ num26.py
@@ -1,3 +1,7 @@
+"""
+Code to covert a user input word to pig latin
+"""
+
 print("....Enter a word you want to convert to pig latin....")
 
 user_input = input("Enter word: ")
@@ -7,3 +11,15 @@ b = user_input.replace(a, '')    # to replace the first letter with nothing
 print(f"{b}{a}ay")                      # adding ay to the end of the word
                                         # I'm now adding the first letter before the ay
 
+
+ # Testing
+"""
+test_word = 'glory'
+print(test_word.replace('g', 'h'))
+print(test_word.replace('g', ''))
+print(test_word.replace(test_word[0], ''))
+
+a = test_word[0]
+b = test_word.replace(a, '')
+print(f"{b}{a}ay")
+"""
