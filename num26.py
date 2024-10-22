52333c4ce2b563fc8773c5685cb9b9ed7065df30
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-22 02:15:10 +0100

    Updated it to add the first letter before the ay

--- num26.py
+++ num26.py
@@ -4,12 +4,15 @@ print("....Enter a word you want to convert to pig latin....")
 # print(test_word.replace('g', 'h'))
 # print(test_word.replace('g', ''))
 # print(test_word.replace(test_word[0], ''))
+#
+# a = test_word[0]
+# b = test_word.replace(a, '')
+# print(f"{b}{a}ay")
 
 user_input = input("Enter word: ")
 
-a = user_input.replace(user_input[0], '')  # to replace the first letter with nothing
-print(f"{a}ay")                                  # adding ay to the end of the word
-
-
-
+a = user_input[0]                      # assigning the first index to a variable to be useable later
+b = user_input.replace(a, '')    # to replace the first letter with nothing
+print(f"{b}{a}ay")                      # adding ay to the end of the word
+                                        # adding the first letter before the ay
 
