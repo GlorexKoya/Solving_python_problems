09f0155e69aef43327349e0e4d561536bafd488a
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-23 02:49:00 +0100

    "Stll Solving question "

---
+++ num42.py
@@ -0,0 +1,24 @@
+total = 0
+
+
+# def get_users_response(question: str) -> bool:
+#     response = input("Do you want this number included?(yes/no) ").lower()
+#     if response == 'yes':
+#         return True
+#     elif response == 'no':
+#         return False
+#     else:
+#         print("Enter yes or no!")
+#
+#
+# user_questions = ["Enter number 1: ",
+#                   "Enter number 2: ",
+#                   "Enter number 3: ",
+#                   "Enter number 4: ",
+#                   "Enter number 5: "]
+
+num_1 = int(input("Enter number 1"))
+num_2 = int(input("Enter number 2"))
+num_3 = int(input("Enter number 3"))
+num_4 = int(input("Enter number 4"))
+num_5 = int(input("Enter number 5"))
