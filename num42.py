91c258f70f71768e9e088677a8ef3555bc56f757
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-24 01:04:00 +0100

    "Finished the code roughly"

--- num42.py
+++ num42.py
@@ -1,39 +1,46 @@
 def get_users_response(question: str) -> bool:
-    response = input("Do you want this number included?(y/n) ").lower()
-    if response == 'y':
-        return True
-    elif response == 'n':
-        return False
-    else:
-        print("Enter y(yes) or n(no)!")
+    while True:
+        response = input(question + "(y/n) ").lower()
+        if response == 'y':
+            return True
+        elif response == 'n':
+            return False
+        else:
+            print("Enter y(yes) or n(no)!")
 
 
-def user_questions(questions: [str]) -> dict[str:int]:
-    questions = ["Enter number 1: ",
-                 "Enter number 2: ",
-                 "Enter number 3: ",
-                 "Enter number 4: ",
-                 "Enter number 5: "]
+def user_questions(questions: list[str]) -> dict[str, int]:
 
     responses = {}
     total = 0
     for question in questions:
-        x = int(input(question))
-        responses[question] = x
-        var = get_users_response("hii")
+        num = int(input(question))
 
-        if var == 'y':
-            total += x
-        elif var == 'n':
-            continue
-        else:
-            pass
+        responses[question] = num
+        # To input them in the dictionary i.e. responses[question] = key and num = value
+
+        var = get_users_response("Do you want this number included in the total?")
+
+        # # total = total + x
+        #
+        # if var == 'y':
+        #     total += x
+        # elif var == 'n':
+        #     total -= x
+        # else:
+        #     pa
+
+        if var:
+            total += num
 
     print(f"The total number is {total}")
+    return responses
 
 
-hii = print("Questions")
-hey = user_questions([hii])
+questions = ["Enter number 1: ", "Enter number 2: ", "Enter number 3: ", "Enter number 4: ", "Enter number 5: "]
+user_questions(questions)
+
+
 
 # total = 0
 # if get_users_response(user_questions(x)) == 'yes':
