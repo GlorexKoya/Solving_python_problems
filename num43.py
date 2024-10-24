4bb5a2ff75860903478c59047b73c3cf11b13ba4
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-24 15:07:52 +0100

    "Code to ask user to input between up and down and then some conditions." "

--- num43.py
+++ num43.py
@@ -5,17 +5,29 @@ If user inputs, down, it would print from the 20 to that number
 """
 
 direction = input("Pick a direction (up/down): ").lower()
+
 if direction == 'up':
     top_num = int(input("   Input top number: "))
     for x in range(1, top_num+1):
-        print(x, end=", ")
-    print()
+
+        """This if block is to avoid the coma after the last number"""
+        if x == top_num:
+            print(x)
+        else:
+            print(x, end=", ")
+    #     print(x, end=", ")
+    # print()
 
     """If the user selects down"""
 elif direction == 'down':
     down_num = int(input("  Input number below 20: "))
     for y in range(20, down_num-1, -1):
-        print(y, end=", ")
+
+        """This if block is to avoid the coma after the last number"""
+        if y == down_num:
+            print(y)
+        else:
+            print(y, end=", ")
 
     """If user enters an invalid input"""
 else:
