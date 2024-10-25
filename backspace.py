11fc85249db9c038104aa79b11340bc8c963d6a1
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-25 05:05:13 +0100

    "Done it!"

--- backspace.py
+++ backspace.py
@@ -2,3 +2,17 @@
 Code to remove fullstops and comas in a word
 """
 
+user_input = input("Enter any word: ")
+stops = ("." or ",")
+
+if user_input.endswith(stops):
+    print(f"{user_input}\b")
+else:
+    print(user_input)
+
+
+# if user_input == word:
+#     print(f"{word}\b")
+# else:
+#     print(user_input)
+
