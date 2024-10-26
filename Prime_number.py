6a9dac969a7350093b91449b1f8bb93c05230398
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-26 22:09:40 +0100

    "Code to check if a number is a prime number"

---
+++ Prime_number.py
@@ -0,0 +1,21 @@
+"""
+Code to check if a users number is a prime number
+A prime number is a number that is cannot be divided by two and can be devided by itself
+Note: 0 and 1 are not prime numbers.
+"""
+
+print()
+number = int(input("Enter a number: "))
+
+if number < 2:
+    print(f"{number} is not a prime number")
+elif number == 2:
+    print(f"{number} is a prime number")
+elif number % 2 == 0:
+    print(f"{number} is not a prime number")
+elif number / number == 1:
+    print(f"{number} is a prime number")
+else:
+    print("Enter a whole number")
+
+
