b6d7b06777bb63b5c68d7d90363232b2472267cb
Oluwakoya Glory <koyaglorex2021@gmail.com>
2024-10-27 22:32:34 +0100

    "Added a while-True loop to repeat question incase of any mistakes from the user"

--- Prime_number.py
+++ Prime_number.py
@@ -5,20 +5,22 @@ Note: 0 and 1 are not prime numbers.
 """
 
 print()
-number = int(input("Enter a number: "))
 print("..Check if number is a prime number..")
+number = int(input("Enter a number: "))
 
-try:
-    if number < 2:
-        print(f"{number} is not a prime number")
-    elif number == 2:
-        print(f"{number} is a prime number")
-    elif number % 2 == 0:
-        print(f"{number} is not a prime number")
-    elif number / number == 1:
-        print(f"{number} is a prime number")
-    else:
-        print("Enter a whole number")
-except ValueError:
-    print("please enter a whole number.")
+while True:
+    try:
+        if number < 2:
+            print(f"{number} is not a prime number")
+            break
+        elif number == 2:
+            print(f"{number} is a prime number")
+        elif number % 2 == 0:
+            print(f"{number} is not a prime number")
+        elif number / number == 1:
+            print(f"{number} is a prime number")
+        else:
+            print("Enter a whole number")
+    except ValueError:
+        print("please enter a whole number.")
 
