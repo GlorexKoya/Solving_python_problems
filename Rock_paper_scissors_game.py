fdde39446fea7ecf97d898787355c468b39b9e66
GlorexKoya <koyaglorex2021@gmail.com>
2024-10-31 22:50:26 +0100

    I modified the variable names

--- Rock_paper_scissors_game.py
+++ Rock_paper_scissors_game.py
@@ -8,44 +8,44 @@ game = f"{'*' * 4}This is a (Rock/Paper/Scissors) game{'*' * 4}\n"
 print(game)
 
 
-variable = ['rock', 'paper', 'scissors']
-computer = rd.choice(variable)
+options = ['rock', 'paper', 'scissors']
+computer = rd.choice(options)
 
-user_guess = input("pick your guess: ").lower()
-if computer == 'rock':
-    if user_guess == 'paper':
-        print(f' my guess is {computer}') 
+users_guess = input("pick your guess: ").lower()
+if computers_choice == 'rock':
+    if users_guess == 'paper':
+        print(f' my guess is {computers_choice}') 
         print(' you win!')
    
-if computer == 'paper':
-    if user_guess == 'scissors':
-        print(f' my guess is {computer}')
+if computers_choice == 'paper':
+    if users_guess == 'scissors':
+        print(f' my guess is {computers_choice}')
         print(' you win!')
             
-if computer == 'scissors':
-    if user_guess == 'rock':
-        print(f' my guess is {computer}')
+if computers_choice == 'scissors':
+    if users_guess == 'rock':
+        print(f' my guess is {computers_choice}')
         print(' you win!')
   
-if computer == user_guess:
-        print(f' my guess is {computer} also')
+if computers_choice == users_guess:
+        print(f' my guess is {computers_choice} also')
         print(" it's a tie =)")
      
-if computer == 'rock':
-    if user_guess == 'scissors':
-        print(f' my guess is {computer}')
+if computers_choice == 'rock':
+    if users_guess == 'scissors':
+        print(f' my guess is {computers_choice}')
         print(' aw, computer wins. ' 
               'Try again :)')
         
-if computer == 'scissors':
-    if user_guess == 'paper':
-        print(f' my guess is {computer}')
+if computers_choice == 'scissors':
+    if users_guess == 'paper':
+        print(f' my guess is {computers_choice}')
         print(' aw, computer wins. ' 
               'Try again :)')
   
-if computer == 'paper':
-    if user_guess == 'rock':
-        print(f' my guess is {computer}')
+if computers_choice == 'paper':
+    if users_guess == 'rock':
+        print(f' my guess is {computers_choice}')
         print(' aw, computer wins. ' 
               'Try again :)')
               
