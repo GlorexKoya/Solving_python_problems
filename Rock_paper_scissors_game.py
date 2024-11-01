c001cb89790aca40e5b14107fa15531b0e504c53
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-01 23:44:52 +0100

    Added a while loop to give user 5 attempts

--- Rock_paper_scissors_game.py
+++ Rock_paper_scissors_game.py
@@ -10,42 +10,45 @@ print(game)
 
 options = ['rock', 'paper', 'scissors']
 computers_choice = rd.choice(options)
+limited_trial = 5 
 
 users_guess = input("pick your guess: ").lower()
-if computers_choice == 'rock':
-    if users_guess == 'paper':
-        print(f' my guess is {computers_choice}') 
-        print(' you win!')
-   
-if computers_choice == 'paper':
-    if users_guess == 'scissors':
-        print(f' my guess is {computers_choice}')
-        print(' you win!')
+while limited_trial > 0:
+
+    if computers_choice == 'rock':
+        if users_guess == 'paper':
+            print(f' my guess is {computers_choice}') 
+            print(' you win!')
+       
+    if computers_choice == 'paper':
+        if users_guess == 'scissors':
+            print(f' my guess is {computers_choice}')
+            print(' you win!')
             
-if computers_choice == 'scissors':
-    if users_guess == 'rock':
-        print(f' my guess is {computers_choice}')
-        print(' you win!')
-  
-if computers_choice == users_guess:
-        print(f' my guess is {computers_choice} also')
-        print(" it's a tie =)")
+    if computers_choice == 'scissors':
+        if users_guess == 'rock':
+            print(f' my guess is {computers_choice}')
+            print(' you win!')
+      
+    if computers_choice == users_guess:
+            print(f' my guess is {computers_choice} also')
+            print(" it's a tie =)")
      
-if computers_choice == 'rock':
-    if users_guess == 'scissors':
-        print(f' my guess is {computers_choice}')
-        print(' aw, computer wins. ' 
-              'Try again :)')
+    if computers_choice == 'rock':
+        if users_guess == 'scissors':
+            print(f' my guess is {computers_choice}')
+            print(' aw, computer wins. ' 
+                  'Try again :)')
         
-if computers_choice == 'scissors':
-    if users_guess == 'paper':
-        print(f' my guess is {computers_choice}')
-        print(' aw, computer wins. ' 
-              'Try again :)')
+    if computers_choice == 'scissors':
+        if users_guess == 'paper':
+            print(f' my guess is {computers_choice}')
+            print(' aw, computer wins. ' 
+                  'Try again :)')
   
-if computers_choice == 'paper':
-    if users_guess == 'rock':
-        print(f' my guess is {computers_choice}')
-        print(' aw, computer wins. ' 
-              'Try again :)')
-              
+    if computers_choice == 'paper':
+        if users_guess == 'rock':
+            print(f' my guess is {computers_choice}')
+            print(' aw, computer wins. ' 
+                  'Try again :)')
+limited_trial += 1        
