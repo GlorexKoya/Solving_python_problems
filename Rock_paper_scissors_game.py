2f5c95d402501717a35421595341e0bae2eb64ef
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-02 01:15:27 +0100

    Added some user-friendly outputs..

--- Rock_paper_scissors_game.py
+++ Rock_paper_scissors_game.py
@@ -11,49 +11,54 @@ print(game)
 initial_trial = 0 
 max_trial = 5
 
+print("..You have five trials..")
 while initial_trial < max_trial:
+    
     options = ['rock', 'paper', 'scissors']
     computers_choice = rd.choice(options)
     
-    users_guess = input("pick your guess: ").lower()
+    users_guess = input(" Pick your guess: ").lower()
  
     if computers_choice == 'rock':
         if users_guess == 'paper':
-            print(f' my guess is {computers_choice}') 
+            print(f'  my guess is {computers_choice}') 
             print(' you win!')
        
     if computers_choice == 'paper':
         if users_guess == 'scissors':
             print(f' my guess is {computers_choice}')
-            print(' you win!')
+            print('  you win!')
             
     if computers_choice == 'scissors':
         if users_guess == 'rock':
-            print(f' my guess is {computers_choice}')
-            print(' you win!')
+            print(f'  my guess is {computers_choice}')
+            print('  you win!')
       
     if computers_choice == users_guess:
-            print(f' my guess is {computers_choice} also')
-            print(" it's a tie =)")
+            print(f'  my guess is {computers_choice} also')
+            print("  it's a tie =)")
      
     if computers_choice == 'rock':
         if users_guess == 'scissors':
-            print(f' my guess is {computers_choice}')
-            print(' aw, computer wins. ' 
+            print(f'  my guess is {computers_choice}')
+            print('  aw, computer wins. ' 
                   'Try again :)')
         
     if computers_choice == 'scissors':
         if users_guess == 'paper':
-            print(f' my guess is {computers_choice}')
-            print(' aw, computer wins. ' 
+            print(f'  my guess is {computers_choice}')
+            print('  aw, computer wins. ' 
                   'Try again :)')
   
     if computers_choice == 'paper':
         if users_guess == 'rock':
-            print(f' my guess is {computers_choice}')
-            print(' aw, computer wins. ' 
-                  'Try again :)')
-    
+            print(f'  my guess is {computers_choice}')
+            print('  aw, computer wins. ' 
+                  'Try again :)') 
+                  
     initial_trial += 1  
-    print()     
-    
+    print() 
+    print(f"..You have {max_trial - initial_trial} more tries..") 
+         
+
+print('Game over!')    
