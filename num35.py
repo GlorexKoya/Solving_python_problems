b05c87a02018593f169a15d70670e0fa486b7979
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-05 22:11:58 +0100

    Added a number input and display the name of user that number of times

--- num35.py
+++ num35.py
@@ -1,11 +1,12 @@
 """
 Code to get the name of a user and display it three times.
+Now we add a number and display the name that number of times
 """
 
-user_input = input("Enter your name: ")
-for i in range(3):
+user_input = input("Enter your name: ").capitalize()
+user_input_num = int(input("Enter a number: "))
+for i in range(user_input_num):
     print(user_input)
-
 
 
 
