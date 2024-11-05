53672613904528a663d44da98557757af4956529
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-05 22:18:02 +0100

    Added a user numbe input to print the letters over again based on users number input

--- num37.py
+++ num37.py
@@ -1,10 +1,15 @@
 """
 Code to display the characters users name on new lines
+Now we add a number input to print the characters on each line
+that number of times based on the number input
 """
 
 user_input = input("Enter your name: ")
+user_input_num = int(input("Enter a number: "))
 
-for i in user_input:
-    print(i)
+for x in range(user_input_num):
+    for i in user_input:
+        print(i)
+    print()
 
 
