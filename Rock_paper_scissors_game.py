970795a49eae4cbab4115cd9a24f4326edc043f7
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-05 23:31:40 +0100

    I removed the result_count function when i discovered it wasnt very useful. And i replaced it with two counters. One for the computer and One for the user

--- Rock_paper_scissors_game.py
+++ Rock_paper_scissors_game.py
@@ -13,21 +13,12 @@ print(f"Hey {users_name}, Welcome to Glorex (Rock/Paper/Scissors) game :)\n"
       f" You have to input between rock, paper or scissors \n"
       f" ..You can begin now..\n")
 
-def result_count(result: str):
-    if result == "WON HERE":
-        return True
-    elif result == "DREW HERE":
-        pass
-    elif result == "lOST HERE":
-        return False
-    else:
-        pass
-
-
 initial_trial = 0
 max_trial = 5
+user_result_counter = 0
+computer_result_counter = 0
 
-print("***..You have five trials..***")
+print("***..You have 5 trials..***")
 while initial_trial < max_trial:
     
     options = ['rock', 'paper', 'scissors']
@@ -39,46 +30,46 @@ while initial_trial < max_trial:
         if users_guess == 'paper':
             print(f'  my guess is {computers_choice}')
             print("  You win!")
-            result_count("WON HERE")
+            user_result_counter += 1
 
     if computers_choice == 'paper':
         if users_guess == 'scissors':
             print(f' my guess is {computers_choice}')
             print("  You win!")
-            result_count("WON HERE")
+            user_result_counter += 1
 
     if computers_choice == 'scissors':
         if users_guess == 'rock':
             print(f'  my guess is {computers_choice}')
             print("  You win!")
-            result_count("WON HERE")
+            user_result_counter += 1
       
     if computers_choice == users_guess:
             print(f'  my guess is {computers_choice} also')
             print("  It's a tie =)")
-            result_count("DREW HERE")
-     
+
     if computers_choice == 'rock':
         if users_guess == 'scissors':
             print(f'  my guess is {computers_choice}')
             print('  Aw, computer wins this round :)')
-            result_count('LOST HERE')
+            computer_result_counter += 1
         
     if computers_choice == 'scissors':
         if users_guess == 'paper':
             print(f'  my guess is {computers_choice}')
             print('  Aw, computer wins this round :)')
-            result_count('LOST HERE')
-  
+            computer_result_counter += 1
+
     if computers_choice == 'paper':
         if users_guess == 'rock':
             print(f'  my guess is {computers_choice}')
             print('  Aw, computer wins this round :)')
-            result_count("LOST HERE")
-                  
+            computer_result_counter += 1
+
     initial_trial += 1  
     print() 
     print(f"..You have {max_trial - initial_trial} more tries..") 
-         
 
-print('Game over!')    
+print('Game over!')
+
+print(f"The final score is {users_name}({user_result_counter}) - Computer({computer_result_counter})")
