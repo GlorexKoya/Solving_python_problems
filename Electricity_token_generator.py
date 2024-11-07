a1999acedec90eed99cd1ba19060a4425a190888
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-07 23:05:58 +0100

    to print the numbers in five places

--- Electricity_token_generator.py
+++ Electricity_token_generator.py
@@ -4,5 +4,9 @@ This code is to generate fake electricity token values using the random module
 
 import random as rd
 
+# 0000-0000-0000-0000-0000         #How the values will look like
 E_token = rd.randint(0, 9999)
 print(E_token)
+
+for num in range(5):
+    print(E_token)
