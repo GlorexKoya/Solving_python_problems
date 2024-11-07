70d832cd6e8427054271d75d30f8dc22be8d04c4
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-07 23:12:54 +0100

    Iterated over the random figure generator to give me new values after each iteration

--- Electricity_token_generator.py
+++ Electricity_token_generator.py
@@ -5,6 +5,7 @@ This code is to generate fake electricity token values using the random module
 import random as rd
 
 # 0000-0000-0000-0000-0000         #How the values will look like
+# Iterated over the random figure generator to give me new values after each iteration
 
 for num in range(5):
     E_token = rd.randint(0, 9999)
