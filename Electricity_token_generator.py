6909a22dac90d80dd6f3079b1eb60907ce7c282e
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-08 00:24:24 +0100

    Added some other information to the users receipt(still under construction..)

--- Electricity_token_generator.py
+++ Electricity_token_generator.py
@@ -4,14 +4,15 @@ This code is to generate fake electricity token values using the random module
 
 import random as rd
 
-# 0000-0000-0000-0000-0000         #How the values will look like
-# Iterated over the random figure generator to give me new values after each iteration
 
 name = input("\nEnter your full name: ").title()
 print("    Number Should be 11 numbers i.e 450123.....")
 meter_number = int(input("Enter your meter number: "))
 meter_type = input("Prepaid or Postpaid? ")
 amount = int(input("Enter amount: "))
+
+# 0000-0000-0000-0000-0000         #How the values will look like
+# Iterated over the random figure generator to give me new values after each iteration
 
 for num in range(5):
     E_token = rd.randint(0, 9999)
@@ -22,7 +23,10 @@ for num in range(5):
 
 
 print(f"\n..................... ELECTRICITY RECEIPT...........................\n"
-      f"Customer Name : {name}\n"
-      f"Meter number : {meter_number}\n",
-      f"Meter Type : {meter_type}\n",
-      f"Amount : ")
+      f"Transaction ID : ",
+      f"\nCustomer Name : {name}",
+      f"\nCustomer Address : {}"
+      f"\nMeter Number: {meter_number}",
+      f"\nMeter Type : {meter_type}",
+      f"\nAmount : {amount}",
+      f"\nTransaction Date{}"),
