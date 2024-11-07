959a0fca0ca78798f77ff2d6a1ace84b44798801
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-07 23:10:55 +0100

    I've generated the dummy electricity-token values now

--- Electricity_token_generator.py
+++ Electricity_token_generator.py
@@ -5,8 +5,8 @@ This code is to generate fake electricity token values using the random module
 import random as rd
 
 # 0000-0000-0000-0000-0000         #How the values will look like
-E_token = rd.randint(0, 9999)
-print(E_token)
 
 for num in range(5):
-    print(E_token)
+    E_token = rd.randint(0, 9999)
+    print(E_token, end="-")
+
