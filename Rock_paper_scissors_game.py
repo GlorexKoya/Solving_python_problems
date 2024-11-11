2afb75ff416cf9edde30a715469d74e28c3f4a9f
GlorexKoya <koyaglorex2021@gmail.com>
2024-11-11 23:39:49 +0100

    Simplified users input to r,p,y for better user-experience

--- Rock_paper_scissors_game.py
+++ Rock_paper_scissors_game.py
@@ -11,7 +11,8 @@ print(game)
 users_name = input("Enter your name: ").capitalize()
 print(f"Hey {users_name}, Welcome to Glorex (Rock/Paper/Scissors) game :)\n"
       f" You have to input between rock, paper or scissors \n"
-      f"  You can begin now!\n")
+      f"  Input (r) for rock, (p) for paper, (s) for scissors\n"
+      f"   You can begin now!\n")
 
 initial_trial = 0
 max_trial = 5
@@ -21,48 +22,53 @@ computer_result_counter = 0
 print("***..You have 5 trials..***")
 while initial_trial < max_trial:
     
-    options = ['rock', 'paper', 'scissors']
+    options = ['r', 'p', 's']
     computers_choice = rd.choice(options)
     
     users_guess = input(" Pick your guess: ").lower()
  
-    if computers_choice == 'rock':
-        if users_guess == 'paper':
-            print(f'  my guess is {computers_choice}')
+    if computers_choice == 'r':
+        if users_guess == 'p':
+            print(f'  my guess is rock')
             print("  You win!")
             user_result_counter += 1
 
-    if computers_choice == 'paper':
-        if users_guess == 'scissors':
-            print(f' my guess is {computers_choice}')
+    if computers_choice == 'p':
+        if users_guess == 's':
+            print(f' my guess is paper')
             print("  You win!")
             user_result_counter += 1
 
-    if computers_choice == 'scissors':
-        if users_guess == 'rock':
-            print(f'  my guess is {computers_choice}')
+    if computers_choice == 's':
+        if users_guess == 'r':
+            print(f'  my guess is scissors')
             print("  You win!")
             user_result_counter += 1
-      
+
     if computers_choice == users_guess:
-            print(f'  my guess is {computers_choice} also')
-            print("  It's a tie =)")
+        if users_guess == 'r':
+            print(f'  my guess is rock also')
+        elif users_guess == 'p':
+            print(f'  my guess is paper also')
+        elif users_guess == 's':
+            print(f'  my guess is scissors also')
+        print("  It's a tie =)")
 
-    if computers_choice == 'rock':
-        if users_guess == 'scissors':
-            print(f'  my guess is {computers_choice}')
+    if computers_choice == 'r':
+        if users_guess == 's':
+            print(f'  my guess is rock')
             print('  Aw, computer wins this round :)')
             computer_result_counter += 1
         
-    if computers_choice == 'scissors':
-        if users_guess == 'paper':
-            print(f'  my guess is {computers_choice}')
+    if computers_choice == 's':
+        if users_guess == 'p':
+            print(f'  my guess is scissors')
             print('  Aw, computer wins this round :)')
             computer_result_counter += 1
 
-    if computers_choice == 'paper':
-        if users_guess == 'rock':
-            print(f'  my guess is {computers_choice}')
+    if computers_choice == 'p':
+        if users_guess == 'r':
+            print(f'  my guess is paper')
             print('  Aw, computer wins this round :)')
             computer_result_counter += 1
 
